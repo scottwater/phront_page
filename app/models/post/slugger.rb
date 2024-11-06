91c1c87baa163fc3c97a269f7a246d440db09e5b
Scott Watermasysk <scottwater@gmail.com>
2024-11-06 10:12:29 -0500

    Clean up Slugger

--- app/models/post/slugger.rb
+++ app/models/post/slugger.rb
@@ -8,21 +8,21 @@ module Post::Slugger
   end
 
   def truncate_raw_slug(raw_slug)
-    raw_slug.truncate(50, omission: '', separator: ' ').strip
+    raw_slug.truncate(50, omission: "", separator: " ").strip
   end
 
   def slugify
     if page_id_changed? && (previous_page = Page.find_by(id: page_id_was))
-      self.slug = slug.sub(previous_page.slug, '')
+      self.slug = slug.sub(previous_page.slug, "")
     end
     # if page slug exists, remove temporarily remove it
-    self.slug = slug.sub(page.slug, '') if page && slug&.start_with?(page.slug)
+    self.slug = slug.sub(page.slug, "") if page && slug&.start_with?(page.slug)
 
     self.slug = truncate_raw_slug(slug.presence || title.presence || SecureRandom.alphanumeric(5).downcase).parameterize
 
     self.slug = "#{page.slug}/#{slug}" if page
 
-    return if slug.starts_with?('/')
+    return if slug.starts_with?("/")
 
     self.slug = "/#{slug}"
   end
