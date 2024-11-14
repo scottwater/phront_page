6b2596ebd335c4c9d0eea7647af2889ea9cf5d78
Scott Watermasysk <scottwater@gmail.com>
2024-11-14 06:55:26 -0500

    Enable Authenticator route constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,41 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  extend Authenticator
   scope module: "admin", path: "admin" do
-    get "/first-run", to: "first_run#new", as: :first_run
-    post "/first-run", to: "first_run#create"
+    authenticate :first_run do
+      get "/first-run", to: "first_run#new", as: :first_run
+      post "/first-run", to: "first_run#create"
+    end
+
     get "sign_in", to: "sessions#new"
     post "sign_in", to: "sessions#create"
-    resources :sessions, only: [:index, :show, :destroy]
-    resources :pages, except: [:show]
-    resources :posts, except: [:show]
-    resources :redirects, except: [:show]
-    resources :configurations
-    get "/orphans/:type", to: "orphans#index", as: "orphaned_items",
-      constraints: {type: /(posts|pages)/}
-    delete "/orphans/:type/:uid", to: "orphans#delete", as: "delete_orphaned_items",
-      constraints: {type: /(post|page)/}
-    get "/posts/close_popup", to: "posts#close_popup", as: "close_popup"
-    match "/previews", to: "previews#show", via: [:patch, :post]
-    get "/revisions/(:revision_type)/(:revision_model_id)", to: "revisions#show"
-    post "/revisions", to: "revisions#create_for_new"
-    patch "/revisions", to: "revisions#create_for_existing"
-    get "/profile" => "profile#edit", :as => "edit_profile"
-    patch "/profile" => "profile#update", :as => "update_profile"
-    get "/account" => "account#edit", :as => "edit_account"
-    patch "/account" => "account#update", :as => "update_account"
-    get "/tools" => "tools#index", :as => "tools"
-    get "/" => "admin#index", :as => :admin_root
+
+    authenticate :author do
+      resources :sessions, only: [:index, :show, :destroy]
+      resources :pages, except: [:show]
+      resources :posts, except: [:show]
+      resources :redirects, except: [:show]
+      resources :configurations
+      get "/orphans/:type", to: "orphans#index", as: "orphaned_items",
+        constraints: {type: /(posts|pages)/}
+      delete "/orphans/:type/:uid", to: "orphans#delete", as: "delete_orphaned_items",
+        constraints: {type: /(post|page)/}
+      get "/posts/close_popup", to: "posts#close_popup", as: "close_popup"
+      match "/previews", to: "previews#show", via: [:patch, :post]
+      get "/revisions/(:revision_type)/(:revision_model_id)", to: "revisions#show"
+      post "/revisions", to: "revisions#create_for_new"
+      patch "/revisions", to: "revisions#create_for_existing"
+      get "/profile" => "profile#edit", :as => "edit_profile"
+      patch "/profile" => "profile#update", :as => "update_profile"
+      get "/account" => "account#edit", :as => "edit_account"
+      patch "/account" => "account#update", :as => "update_account"
+      get "/tools" => "tools#index", :as => "tools"
+      get "/" => "admin#index", :as => :admin_root
+    end
+
+    get "*path", to: redirect("/admin/sign_in")
+    get "/", to: redirect("/admin/sign_in")
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
